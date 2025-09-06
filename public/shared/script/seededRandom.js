75860752006169fe0f534381f19e21d4373450b8
WarBuggy <hovanbuu@gmail.com>
2025-09-06 16:28:46 -0700

    * Add SeededRandom class

---
+++ public/shared/script/seededRandom.js
@@ -0,0 +1,26 @@
+export class SeededRandom {
+    constructor(input) {
+        const { seed, } = input;
+        this.m = 0x80000000;        // 2**31
+        this.a = 1664525;           // LCG multiplier
+        this.c = 1013904223;        // LCG increment
+        this.seed = seed || Math.floor(Math.random() * (this.m - 1));
+        this.state = this.seed;      // current state
+    }
+
+    // Returns a pseudo-random number between 0 (inclusive) and 1 (exclusive)
+    next() {
+        this.state = (this.a * this.state + this.c) % this.m;
+        return this.state / (this.m - 1);
+    }
+
+    // Returns an integer between min and max (inclusive)
+    nextInt(min, max) {
+        return Math.floor(this.next() * (max - min + 1)) + min;
+    }
+
+    // Optionally, reset the RNG to its original seed
+    reset() {
+        this.state = this.seed;
+    }
+}
